deb7c8086f3cebea6c61c40bc174a8783ded7fbc
Kotaro Inoue <k.inoue@effect-effect.com>
2021-01-18 23:55:00 +0900

    ウィンドウ位置の自動設定対応

---
+++ .config/devilspie2/adjust_in_home.lua
@@ -0,0 +1,15 @@
+if (get_class_instance_name()=="code") then
+  set_window_geometry2 (59, 1467, 1861, 1053)
+end
+
+if (get_class_instance_name()=="Navigator") then
+  set_window_geometry2 (1, 0, 1523, 1440)
+end
+
+if (get_class_instance_name()=="terminator") then
+  set_window_geometry2 (0, 791, 2557, 649)
+end
+
+if (get_class_instance_name()=="microsft teams - preview") then
+  set_window_geometry2 (1524, 0, 1036, 821)
+end
